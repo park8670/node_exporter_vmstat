e7f3799cea7d665282d938a48c5169ec3090209c
park8670 <139953551+park8670@users.noreply.github.com>
2025-05-27 09:41:25 +0900

    Update cat_linux.go
    Signed-off-by: park8670 <139953551+park8670@users.noreply.github.com>

--- collector/cat_linux.go
+++ collector/cat_linux.go
@@ -1,1 +1,55 @@
 
+package collector
+
+import (
+	"bufio"
+	"os"
+	"strings"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type catCollector struct {
+	info *prometheus.Desc
+}
+
+func init() {
+	registerCollector("cat", defaultEnabled, NewCatCollector)
+}
+
+func NewCatCollector() (Collector, error) {
+	return &catCollector{
+		info: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "cat", "info"),
+			"Key-value info read from /test.txt",
+			[]string{"key", "value"},
+			nil,
+		),
+	}, nil
+}
+
+func (c *catCollector) Update(ch chan<- prometheus.Metric) error {
+	file, err := os.Open("/test.txt")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.SplitN(line, ":", 2)
+		if len(parts) != 2 {
+			continue // skip malformed line
+		}
+		key := strings.TrimSpace(parts[0])
+		value := strings.TrimSpace(parts[1])
+		ch <- prometheus.MustNewConstMetric(
+			c.info,
+			prometheus.GaugeValue,
+			1,
+			key, value,
+		)
+	}
+	return scanner.Err()
+}
